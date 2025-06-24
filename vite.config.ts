1ef0dca798ce8015c03115898c631a86ba7fd688
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-06-23 18:04:20 -0600

    config coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { coverageConfigDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,9 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+    coverage: {
+      exclude: ["src/main.tsx", ...coverageConfigDefaults.exclude],
+      reporter: ["text"],
+    },
   },
 });
