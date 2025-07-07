033a2f6b7a3f76e3182b818d9b6376c9d73df545
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-07-07 08:33:36 -0600

    refactor: filtering logic

--- src/components/BrowserExtensions.tsx
+++ src/components/BrowserExtensions.tsx
@@ -3,7 +3,10 @@ import { ExtensionCard } from "./ExtensionCard";
 import data from "../mock/data.json";
 
 export const BrowserExtensions = () => {
-  const [filteredExtensions, setFilteredExtensions] = useState([...data]);
+  const [extensions, setExtensions] = useState([...data]);
+  const [extensionStatusFilter, setExtensionStatusFilter] = useState<
+    "all" | boolean
+  >("all");
 
   return (
     <>
@@ -11,28 +14,24 @@ export const BrowserExtensions = () => {
         <h1>Extensions List</h1>
 
         <div>
-          <button onClick={() => setFilteredExtensions([...data])}>All</button>
-          <button
-            onClick={() =>
-              setFilteredExtensions(data.filter((ext) => ext.isActive))
-            }
-          >
-            Active
-          </button>
-          <button
-            onClick={() =>
-              setFilteredExtensions(data.filter((ext) => !ext.isActive))
-            }
-          >
+          <button onClick={() => setExtensionStatusFilter("all")}>All</button>
+          <button onClick={() => setExtensionStatusFilter(true)}>Active</button>
+          <button onClick={() => setExtensionStatusFilter(false)}>
             Inactive
           </button>
         </div>
       </div>
 
       <ul className="grid grid-cols-3 gap-3.5">
-        {filteredExtensions.map((ext) => (
-          <ExtensionCard key={ext.name} extension={ext} />
-        ))}
+        {extensions
+          .filter(
+            (ext) =>
+              extensionStatusFilter === ext.isActive ||
+              extensionStatusFilter === "all"
+          )
+          .map((ext) => (
+            <ExtensionCard key={ext.name} extension={ext} />
+          ))}
       </ul>
     </>
   );
