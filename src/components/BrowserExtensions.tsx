8236392ff33f744826590236e05c35cdf584a7cf
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-07-07 18:09:14 -0600

    adjust main content styles

--- src/components/BrowserExtensions.tsx
+++ src/components/BrowserExtensions.tsx
@@ -14,14 +14,27 @@ export const BrowserExtensions = () => {
   };
 
   return (
-    <>
-      <div>
-        <h1>Extensions List</h1>
+    <main className="mx-auto grid max-w-[75.625rem] gap-9 px-5">
+      <div className="flex items-center justify-between">
+        <h1 className="text-[2rem] font-bold">Extensions List</h1>
 
-        <div>
-          <button onClick={() => setExtensionStatusFilter("all")}>All</button>
-          <button onClick={() => setExtensionStatusFilter(true)}>Active</button>
-          <button onClick={() => setExtensionStatusFilter(false)}>
+        <div className="flex gap-2.5">
+          <button
+            className="pill-button"
+            onClick={() => setExtensionStatusFilter("all")}
+          >
+            All
+          </button>
+          <button
+            className="pill-button"
+            onClick={() => setExtensionStatusFilter(true)}
+          >
+            Active
+          </button>
+          <button
+            className="pill-button"
+            onClick={() => setExtensionStatusFilter(false)}
+          >
             Inactive
           </button>
         </div>
@@ -32,7 +45,7 @@ export const BrowserExtensions = () => {
           .filter(
             (ext) =>
               extensionStatusFilter === ext.isActive ||
-              extensionStatusFilter === "all"
+              extensionStatusFilter === "all",
           )
           .map((ext) => (
             <ExtensionCard
@@ -42,6 +55,6 @@ export const BrowserExtensions = () => {
             />
           ))}
       </ul>
-    </>
+    </main>
   );
 };
